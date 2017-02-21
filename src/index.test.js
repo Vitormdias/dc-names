59ec29ccd4d70a9316dba47b8b2d97a2ab68c485
Vitor Manfré <vitormd97@gmail.com>
2017-02-21 00:15:33 -0300

    lib tests added

--- src/index.test.js
+++ src/index.test.js
@@ -2,7 +2,21 @@ const expect = require('chai').expect;
 const dcNames = require('./index');
 
 describe('dc-names' , () => {
-    it('should work' , () => {
-        expect(true).to.be.true;
+
+  describe('all' , () => {
+    it('should be a string array' , () => {
+      expect(dcNames.all).to.satisfy(isArrayOfStrings);
+
+      function isArrayOfStrings(array) {
+        return array.every(it => typeof it == 'string');
+      }
     });
+  });
+
+  describe('random' , () => {
+    it('should bring a name from dcNames.all' , () => {
+      expect(dcNames.all).to.include.members([dcNames.random()]);
+    });
+  });
+
 });
