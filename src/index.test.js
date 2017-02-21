0073c3f5c0bd95b2ca504ac5f7e7b471c8857902
Vitor Manfré <vitormd97@gmail.com>
2017-02-21 00:02:51 -0300

    test initial structure added

---
+++ src/index.test.js
@@ -0,0 +1,8 @@
+const expect = require('chai').expect;
+const dcNames = require('./index');
+
+describe('dc-names' , () => {
+    it('should work' , () => {
+        expect(true).to.be.true;
+    });
+});
